7b80d0a050ae1271a01a57023415f889e65d1a37
ByeongDeokLee <bdlee6710@gmail.com>
2025-04-23 17:57:23 +0900

    25/04/23 -2

--- components/KakaoMap.vue
+++ components/KakaoMap.vue
@@ -1,17 +1,47 @@
 <template>
   <div class="kakao-wrap">
-    <input
-      v-model="address"
-      placeholder="주소를 입력하세요"
-      @keydown.enter="searchAddress"
-      class="address-input"
-    />
     <div id="map" class="map-box"></div>
-    <button @click="roadViewFun">로드 뷰</button>
+    <button class="open-sidebar-btn" @click="sidebarOpen = !sidebarOpen">
+      {{ sidebarOpen ? "닫기" : "메뉴" }}
+    </button>
+
+    <!-- 슬라이딩 사이드바 -->
+    <div class="sidebar" :class="{ open: sidebarOpen }">
+      <button class="close-btn" @click="sidebarOpen = false">✕</button>
+      <h3>기능 선택</h3>
+      <button @click="searchAddFun">검색</button>
+      <button @click="directionsFun">길찾기</button>
+      <button @click="busInfoFun">버스</button>
+      <button @click="roadViewFun">로드 뷰</button>
+
+      <div v-if="searchAddYn">
+        <input
+          v-model="address"
+          placeholder="주소를 입력하세요"
+          @keydown.enter="searchAddress"
+          class="address-input"
+        />
+      </div>
+
+      <div v-if="directionsYn" class="direction-box">
+        <input
+          v-model="startName"
+          placeholder="출발지를 입력하세요"
+          class="address-input"
+        />
+        <input
+          v-model="endName"
+          placeholder="도착지를 입력하세요"
+          @keydown.enter="searchDirection"
+          class="address-input"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import axios from "axios";
 import { ref, onMounted } from "vue";
 
 // 카카오 전역 타입 선언
@@ -23,16 +53,68 @@ declare global {
 
 const map = ref<any>(null);
 const marker = ref<any>(null);
-const address = ref("서울특별시 중구 세종대로 110"); // 기본 주소
+const infowindow = ref<any>(null);
+//메뉴
+const directionsYn = ref(false);
+const searchAddYn = ref(true);
+//슬라이딩 도어
+const sidebarOpen = ref(false);
 
+const address = ref("서울특별시 중구 세종대로 110");
+const startName = ref("");
+const endName = ref("");
+
+type ClickedLocationData = {
+  lat: number;
+  lng: number;
+  roadAddress: string;
+  jibunAddress: string;
+};
+
+// //맵클릭
+// function mapClickFun(data: ClickedLocationData) {
+//   console.log("지도 클릭 정보:", data);
+
+//   // 예: address.value = data.roadAddress;
+// }
+
+//버스 정보
+const busInfoFun = async () => {
+  try {
+    const url = "https://ws.bus.go.kr/api/rest/buspos/getBusPosByRtid";
+    const respone = await axios.get(url);
+
+    return respone.data;
+  } catch (error) {}
+};
+
+//로드 뷰
 const roadViewFun = () => {
-  console.log("진입", marker.value.getPosition());
+  const width = 500;
+  const height = 500;
+
+  const left = window.screenX + (window.outerWidth - width) / 2;
+  const top = window.screenY + (window.outerHeight - height) / 2;
+
   window.open(
     `https://map.kakao.com/link/roadview/${marker.value.getPosition().Ma},${
       marker.value.getPosition().La
     }`,
-    "newWindow"
+    "newWindow",
+    `width=${width},height=${height},top=${top},left=${left},scrollbars=yes`
   );
+};
+
+//길찾기
+const directionsFun = () => {
+  directionsYn.value = true;
+  searchAddYn.value = false;
+};
+
+//검색
+const searchAddFun = () => {
+  searchAddYn.value = true;
+  directionsYn.value = false;
 };
 
 onMounted(() => {
@@ -49,14 +131,63 @@ onMounted(() => {
 
     map.value = new window.kakao.maps.Map(container, options);
 
-    // 초기 마커 생성 (비어있을 수 있음)
     marker.value = new window.kakao.maps.Marker({
       map: map.value,
     });
 
+    // 지도 클릭 이벤트 등록
+    window.kakao.maps.event.addListener(
+      map.value,
+      "click",
+      (mouseEvent: any) => {
+        console.log("\n\n 확인 \n\n", !infowindow.value);
+        if (infowindow.value) {
+          infowindow.value.close();
+        }
+        const coords = mouseEvent.latLng;
+
+        const geocoder = new window.kakao.maps.services.Geocoder();
+        geocoder.coord2Address(
+          coords.getLng(),
+          coords.getLat(),
+          (result: any, status: string) => {
+            if (status === window.kakao.maps.services.Status.OK) {
+              // const roadAddr = result[0]?.road_address?.address_name || "";
+              // const jibunAddr = result[0]?.address?.address_name || "";
+              infowindow.value = new window.kakao.maps.InfoWindow({
+                zindex: 1,
+              });
+
+              var detailAddr = !!result[0].road_address
+                ? "<div>도로명주소 : " +
+                  result[0].road_address.address_name +
+                  "</div>"
+                : "";
+              detailAddr +=
+                "<div>지번 주소 : " + result[0].address.address_name + "</div>";
+
+              var content =
+                '<div class="bAddr">' +
+                '<span class="title">법정동 주소정보</span>' +
+                detailAddr +
+                "</div>";
+              infowindow.value.setContent(content);
+              infowindow.value.open(map.value, marker.value);
+            }
+          }
+        );
+
+        // 마커 위치 갱신
+        marker.value.setPosition(coords);
+        marker.value.setMap(map.value);
+      }
+    );
+
     searchAddress();
   });
 });
+
+function searchDirection() {}
 
 function searchAddress() {
   const geocoder = new window.kakao.maps.services.Geocoder();
@@ -64,13 +195,9 @@ function searchAddress() {
   geocoder.addressSearch(address.value, (result: any[], status: string) => {
     if (status === window.kakao.maps.services.Status.OK) {
       const coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
-
-      // 지도 중심 이동
       map.value.setCenter(coords);
-
-      // 마커 위치 재설정
       marker.value.setPosition(coords);
-      //클리 이벤트 등록
+
       window.kakao.maps.event.addListener(marker.value, "click", () => {
         const info = new window.kakao.maps.InfoWindow({
           content: `<div style="padding:8px;"> ${address.value}</div>`,
@@ -89,12 +216,12 @@ function searchAddress() {
 
 <style scoped>
 .kakao-wrap {
-  max-width: 600px;
-  margin: 30px auto;
+  max-width: 100%;
+  position: relative;
 }
 .map-box {
   width: 100%;
-  height: 400px;
+  height: 500px;
   border-radius: 8px;
   margin-top: 12px;
 }
@@ -104,5 +231,41 @@ function searchAddress() {
   border: 1px solid #ccc;
   border-radius: 6px;
   font-size: 16px;
+  margin-bottom: 10px;
+}
+.open-sidebar-btn {
+  margin-top: 10px;
+  padding: 8px 16px;
+}
+.sidebar {
+  position: fixed;
+  top: 0;
+  right: -300px;
+  width: 280px;
+  height: 100vh;
+  background-color: #f8f8f8;
+  border-left: 1px solid #ddd;
+  padding: 20px;
+  box-shadow: -2px 0 5px rgba(0, 0, 0, 0.1);
+  transition: right 0.3s ease-in-out;
+  z-index: 999;
+}
+.sidebar.open {
+  right: 0;
+}
+.sidebar h3 {
+  margin-bottom: 10px;
+}
+.direction-box input {
+  margin-top: 10px;
+}
+.close-btn {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background: transparent;
+  border: none;
+  font-size: 18px;
+  cursor: pointer;
 }
 </style>
